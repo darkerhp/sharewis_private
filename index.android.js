d71265ad87649873cd52c9e765abebdce79e6440
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-07-21 15:50:05 +0900

    fix jest test

--- index.android.js
+++ index.android.js
@@ -4,9 +4,40 @@
  * @flow
  */
 
+import React, { Component } from 'react';
 import {
   AppRegistry,
+  StyleSheet,
+  Text,
+  View
 } from 'react-native';
-import SharewisActMobile from 'js/index'
+import Home from 'src/home'
+
+class SharewisActMobile extends Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 AppRegistry.registerComponent('SharewisActMobile', () => SharewisActMobile);
