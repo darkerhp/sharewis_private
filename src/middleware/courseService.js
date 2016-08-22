85b67becd03c8e4a97f54bfa949e7c7899db09fa
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-08-22 17:42:26 +0900

    fix eslint middleware

--- src/middleware/courseService.js
+++ src/middleware/courseService.js
@@ -1,7 +1,11 @@
 /**
  * @flow
  */
-import { errorOnReceivingMoving, retrievedCourses } from '../actions/courses'
+/* global fetch */
+import {
+  errorOnReceivingMoving,
+  // retrievedCourses,
+} from '../actions/courses';
 
 const API_URL = '';
 const API_KEYS = [
@@ -12,24 +16,23 @@ const getUrlForQuery = (query: string, pageNumber: number, queryNumber: number):
   const apiKey = API_KEYS[queryNumber % API_KEYS.length];
   if (query) {
     return (
-    API_URL + 'courses.json?apikey=' + apiKey + '&q=' +
-    encodeURIComponent(query) + '&page_limit=20&page=' + pageNumber
-    );
-  } else {
-    // With no query, load latest courses
-    return (
-    API_URL + 'lists/courses/in_theaters.json?apikey=' + apiKey +
-    '&page_limit=20&page=' + pageNumber
+      `${API_URL}courses.json?apikey=${apiKey}&q=\
+      ${encodeURIComponent(query)}&page_limit=20&page=${pageNumber}`
     );
   }
-}
+  // With no query, load latest courses
+  return (
+    `${API_URL}...`
+  );
+};
+
 
 class CourseService {
-  constructor(){
+  constructor() {
     this.queryNumber = 0;
   }
 
-  fetchCourses(filter: string, dispatch: any) {
+  fetchCourses() {
     return null;
   }
 
@@ -39,9 +42,9 @@ class CourseService {
 
     fetch(url)
       .then((response) => response.json())
-      .catch((error) => {
+      .catch(() => {
         dispatch(errorOnReceivingMoving());
-      })
+      });
     return null;
   }
 }
