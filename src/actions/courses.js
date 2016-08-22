8cf495bd1fc68f82b16b73ae51bbe603fdea8c3d
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-08-22 17:56:30 +0900

    fix eslint actions

--- src/actions/courses.js
+++ src/actions/courses.js
@@ -1,58 +1,41 @@
 /**
  * @flow
  */
-export const FETCHING_COURSES = "FETCHING_COURSES";
-export const FETCHING_NEXT_PAGE_COURSES = "FETCHING_NEXT_PAGE_COURSES";
-export const RECEIVED_DATA = "RECEIVED_DATA";
-export const ERROR_GETTING_COURSES = "ERROR_GETTING_COURSES";
+import courseService from '../middleware/courseService';
 
-import courseService from '../middleware/courseService'
+export const FETCHING_COURSES = 'FETCHING_COURSES';
+export const FETCHING_NEXT_PAGE_COURSES = 'FETCHING_NEXT_PAGE_COURSES';
+export const RECEIVED_DATA = 'RECEIVED_DATA';
+export const ERROR_GETTING_COURSES = 'ERROR_GETTING_COURSES';
 
-export const fetchCourses = (): Object => {
-  return {
-    type: FETCHING_COURSES
-  }
-}
+export const fetchCourses = (): Object => ({
+  type: FETCHING_COURSES,
+});
 
-export const fetchingNextPageCourses = (): Object => {
-  return {
-    type: FETCHING_NEXT_PAGE_COURSES
-  }
-}
+export const fetchingNextPageCourses = (): Object => ({
+  type: FETCHING_NEXT_PAGE_COURSES,
+});
 
-export const searchCourses = (filter: string): Function => {
-  return (dispatch) => {
+export const searchCourses = (filter: string): Function =>
+  (dispatch) => {
     dispatch(fetchCourses());
-    //courseService.fetchCourses(filter, dispatch);
+    courseService.fetchCourses(filter, dispatch);
   };
-};
 
-export const getCoursesNextPage = (filter: string): Function => {
-  return (dispatch) => {
+export const getCoursesNextPage = (filter: string): Function =>
+  (dispatch) => {
     dispatch(fetchingNextPageCourses());
     courseService.getNextPage(filter, dispatch);
   };
-};
 
-export const errorOnReceivingMoving = (): Object => {
-  return {
-    type: ERROR_GETTING_COURSES,
-  };
-};
+export const errorOnReceivingMoving = (): Object => ({
+  type: ERROR_GETTING_COURSES,
+});
 
-export const retrievedCourses = (data: Object): Object => {
-  return {
-    type: RECEIVED_DATA,
-    data
-  };
-};
+export const retrievedCourses = (data: Object): Object => ({
+  type: RECEIVED_DATA,
+  data,
+});
 
-export const fetchData = (): Function => {
-  return (dispatch) => {
-    dispatch(requestData());
-    return setTimeout(() => {
-      const data = {message: "Hello"};
-      dispatch(receiveData(data));
-    }, 300);
-  };
-};
+export const fetchData = (): Function =>
+  setTimeout(() => 'something', 300);
