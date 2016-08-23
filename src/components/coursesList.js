7ecfa0bae3516dfa1752890d5993f301841993d5
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-08-23 14:57:19 +0900

    add coursesList component

---
+++ src/components/coursesList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactNative from 'react-native';
+
+const {
+  View,
+  Text,
+} = ReactNative;
+
+const CoursesList = () =>
+  <View>
+    <Text>Courses List</Text>
+  </View>;
+
+
+export default CoursesList;
