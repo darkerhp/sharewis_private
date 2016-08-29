98477535b5dc2bcbddfd470bf1a2d2efb9808850
norikt <norikt@gmail.com>
2016-08-29 11:40:31 +0900

    Add actions and ActionTypes Move to constants (Redux official examples style)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,15 @@
-import * as ActionTypes from './ActionTypes';
+import * as types from '../constants/ActionTypes';
 
-export const exampleAction1 = (): ReactElement => ({ type: ActionTypes.EXAMPLE_ACTION1 });
-export const exampleAction2 = (): ReactElement => ({ type: ActionTypes.EXAMPLE_ACTION2 });
+// Onboarding
+export const onScroll = (): ReactElement => ({
+  type: types.ON_SCROLL,
+});
+export const pressPrev = (): ReactElement => ({
+  type: types.PRESS_PREV,
+});
+export const pressNext = (): ReactElement => ({
+  type: types.PRESS_NEXT,
+});
+export const navigateToInitPage = (): ReactElement => ({
+  type: types.NAVIGATE_TO_INIT_PAGE,
+});
---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,5 @@
+// Onboarding
+export const ON_SCROLL = 'ON_SCROLL';
+export const PRESS_PREV = 'PRESS_PREV';
+export const PRESS_NEXT = 'PRESS_NEXT';
+export const NAVIGATE_TO_INIT_PAGE = 'NAVIGATE_TO_INIT_PAGE';
