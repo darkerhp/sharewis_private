323fc2dee59755a71a313837466e969df1b97c7e
norikt <norikt@gmail.com>
2016-08-30 12:57:40 +0900

    Change flow type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
 import * as types from '../constants/ActionTypes';
 
 // Onboarding
-export const onScroll = (): ReactElement => ({
+export const onScroll = (): Object => ({
   type: types.ON_SCROLL,
 });
-export const pressPrev = (): ReactElement => ({
+export const pressPrev = (): Object => ({
   type: types.PRESS_PREV,
 });
-export const pressNext = (): ReactElement => ({
+export const pressNext = (): Object => ({
   type: types.PRESS_NEXT,
 });
-export const navigateToInitPage = (): ReactElement => ({
+export const navigateToInitPage = (): Object => ({
   type: types.NAVIGATE_TO_INIT_PAGE,
 });
