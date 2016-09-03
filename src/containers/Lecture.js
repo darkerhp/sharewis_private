31b6bab5f4c88f575c3253f1d21be3a638dc763b
norikt <norikt@gmail.com>
2016-09-03 17:05:24 +0900

    Tidy

--- src/containers/Lecture.js
+++ src/containers/Lecture.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactNative from 'react-native';
-
-import Video from 'react-native-video';
-
-
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import Video from 'react-native-video';
+
 import * as Actions from '../actions/index';
 import VideoControls from '../components/Lecture/VideoControls';
 
 const { PropTypes } = React;
-const { View, StyleSheet, Text, Dimensions, Image, TouchableOpacity, Animated } = ReactNative;
-
-const lectures = [
-  {
-    type: 'Video',
-    url: 'http://embed.wistia.com/deliveries/442c0200e6412dc5fbf26d3f89dc9bfa8fd4e76c.bin',
-
-  },
-];
+const { View, StyleSheet, Text, TouchableOpacity } = ReactNative;
 
 const styles = StyleSheet.create({
   backgroundVideo: {
@@ -55,9 +45,8 @@ const styles = StyleSheet.create({
     width: 250,
     height: 300,
     resizeMode: 'stretch',
-  }
+  },
 });
-
 
 class Lecture extends React.Component {
   constructor(props) {
@@ -66,7 +55,7 @@ class Lecture extends React.Component {
   }
 
   handlePressPlay() {
-    this.props.actions.pressPlay();
+    this.props.pressPlay();
   }
 
   render() {
@@ -75,32 +64,33 @@ class Lecture extends React.Component {
         <View style={styles.videoContainer}>
           <Video
             source={{ uri: 'http://embed.wistia.com/deliveries/442c0200e6412dc5fbf26d3f89dc9bfa8fd4e76c.bin' }} // Can be a URL or a local file.
-            rate={1.0}                   // 0 is paused, 1 is normal.
-            volume={1.0}                 // 0 is muted, 1 is normal.
-            muted={false}                // Mutes the audio entirely.
-            paused={this.props.isPaused}               // Pauses playback entirely.
-            resizeMode="contain"           // Fill the whole screen at aspect ratio.
-            repeat                // Repeat forever.
-            playInBackground={false}     // Audio continues to play when aentering background.
-            playWhenInactive={false}     // [iOS] Video continues to play whcontrol or notification center are shown.
+            rate={1.0}
+            volume={1.0}
+            muted={false}
+            paused={this.props.isPaused}
+            resizeMode="contain"
+            repeat={false}
+            playInBackground={false}
+            playWhenInactive={false}
             onError={e => console.log(e)}
             style={styles.backgroundVideo}
           />
         </View>
         <View style={{ flex: 1.5, backgroundColor: 'white' }}>
+          {/* TODO ProgressBar 実装する */}
           <View style={{ flex: 0.5, justifyContent: 'flex-end', alignItems: 'stretch' }}>
-            {/* TODO 実装する */}
             <Text>ProgressBar</Text>
           </View>
+          {/* TODO LecturteTitle 実装する */}
           <View style={{ flex: 0.5, justifyContent: 'flex-end', alignItems: 'stretch' }}>
-            {/* TODO 実装する */}
-            <Text>Lecturte Title</Text>
+            <Text>LecturteTitle</Text>
           </View>
           <VideoControls
             isPaused={this.props.isPaused}
-            onPressPlay={this.handlePressPlay}/>
+            onPressPlay={this.handlePressPlay}
+          />
+          {/* TODO NextLecture 実装する */}
           <View style={{ flex: 3, justifyContent: 'flex-end', alignItems: 'stretch' }}>
-            {/* TODO 実装する */}
             <TouchableOpacity
               style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: '#579eff', minHeight: 60 }}
             >
@@ -113,15 +103,20 @@ class Lecture extends React.Component {
   }
 }
 
+Lecture.propTypes = {
+  isPaused: PropTypes.bool.isRequired,
+  pressPlay: PropTypes.func.isRequired,
+};
+
 const mapStateToProps = (state) => ({
   isPaused: state.lecture.isPaused,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(Actions, dispatch),
-});
-
-Lecture.propTypes = {};
-Lecture.defaultProps = {};
+const mapDispatchToProps = (dispatch) => {
+  const actions = bindActionCreators(Actions, dispatch);
+  return ({
+    pressPlay: actions.pressPlay,
+  });
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Lecture);
