a083044e2a34ec6e48204409c3840e8769d1e123
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-09-05 11:12:27 +0900

    fix android build error

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,8 @@
 /**
  * @flow
  */
-import './ReactotronConfig';
 import { AppRegistry } from 'react-native';
+import './ReactotronConfig';
 import Root from './src/containers/Root';
 
 AppRegistry.registerComponent('SharewisActMobile', () => Root);
