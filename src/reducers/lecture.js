047f18d36ac629558fe95b4b856d1e8c1bbfb328
norikt <norikt@gmail.com>
2016-09-05 15:30:35 +0900

    Add press rate case

--- src/reducers/lecture.js
+++ src/reducers/lecture.js
@@ -3,9 +3,11 @@ import * as types from '../constants/ActionTypes';
 
 const initialState = {
   isPaused: false,
-  speed: 1.0, // 1.2, 1.5, 2.0
+  rate: 1,
   isFullScreen: false,
 };
+
+const rates = [1, 1.2, 1.5, 2];
 
 const lecture = (state = initialState, action) => {
   switch (action.type) {
@@ -14,6 +16,14 @@ const lecture = (state = initialState, action) => {
         ...state,
         isPaused: !state.isPaused,
       };
+    case types.PRESS_RATE: {
+      let idx = rates.indexOf(state.rate);
+      idx = (idx === 3) ? 0 : idx + 1;
+      return {
+        ...state,
+        rate: rates[idx],
+      };
+    }
     default:
       return state;
   }
