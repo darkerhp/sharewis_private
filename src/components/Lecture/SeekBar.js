533a07054dfc5acbb9ef65d434dbf3f59aade12a
norikt <norikt@gmail.com>
2016-09-06 16:38:42 +0900

    Add seekbar components

--- src/components/Lecture/SeekBar.js
+++ src/components/Lecture/SeekBar.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactNative from 'react-native';
 
-import Button from 'react-native-button';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 import moment from 'moment';
-require('moment-duration-format');
-
-import BaseStyles from '../../baseStyles';
+import momentDurationFormat from 'moment-duration-format'; // eslint-disable-line
 
 const { PropTypes } = React;
 const { View, StyleSheet, Text, Slider, Dimensions } = ReactNative;
@@ -16,59 +12,53 @@ const DEVICE_WIDTH = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'stretch',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     flexDirection: 'row',
   },
   slider: {
-    width: DEVICE_WIDTH - 132,
+    width: (DEVICE_WIDTH - 90) * 0.9,
   },
-  timeStart: {
+  timeTextContainer: {
     justifyContent: 'center',
-    marginTop: 16,
-    marginLeft: 10,
-    marginRight: 10,
     width: 45,
-    height: 7,
   },
-  timeEnd: {
-    justifyContent: 'center',
-    marginTop: 16,
-    marginLeft: 10,
-    marginRight: 10,
-    width: 45,
-    height: 7,
-  },
-  timeStartText: {
-    fontSize: 13,
-    opacity: 0.6,
-    textAlign: 'center',
-  },
-  timeEndText: {
+  timeText: {
     fontSize: 13,
     opacity: 0.6,
     textAlign: 'center',
   },
 });
 
-const SeekBar = ({ currentTime, duration, onValueChange }) =>
-  <View style={styles.container}>
-    <View style={styles.timeStart}>
-      <Text
-        style={styles.timeStartText}>{moment.duration(currentTime, 'seconds').format('mm:ss', { trim: false })}</Text>
+const SeekBar = ({ currentTime, duration, onValueChange }) => {
+  const timeEnd = duration - currentTime;
+  return (
+    <View style={styles.container}>
+      <View style={styles.timeTextContainer}>
+        <Text style={styles.timeText}>
+          {moment.duration(currentTime, 'seconds').format('mm:ss', { trim: false })}
+        </Text>
+      </View>
+      <Slider
+        maximumValue={duration}
+        value={currentTime}
+        onValueChange={(value) => onValueChange(value)}
+        style={styles.slider}
+      />
+      <View style={styles.timeTextContainer}>
+        <Text style={styles.timeText}>
+          -{moment.duration(timeEnd < 0 ? 0 : timeEnd, 'seconds').format('mm:ss', { trim: false })}
+        </Text>
+      </View>
     </View>
-    <Slider maximumValue={duration} value={currentTime} onValueChange={(value) => this.refs.video.seek(value)}
-            style={styles.slider}/>
-    <View style={styles.timeEnd}>
-      <Text
-        style={styles.timeEndText}>-{moment.duration(duration - currentTime, 'seconds').format('mm:ss', { trim: false })}</Text>
-    </View>
-  </View>;
+  );
+};
+
 
 SeekBar.propTypes = {
   currentTime: PropTypes.number.isRequired,
-  duration: PropTypes.string.isRequired,
-  handleValueChange: PropTypes.func.isRequired,
+  duration: PropTypes.number.isRequired,
+  onValueChange: PropTypes.func.isRequired,
 };
 
 export default SeekBar;
