e12e2565b41ce5779031831f82a34d36c07e0f50
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-09-07 11:35:34 +0900

    install redux-persist and set storage in store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,14 @@
+/* global __DEV__ */
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint no-console: ["error", { allow: ["log"] }] */
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import Reactotron from 'reactotron-react-native';
 import createReactotronEnhancer from 'reactotron-redux';
-import rootReducer from '../reducers';
+import { persistStore, autoRehydrate } from 'redux-persist';
 
+import rootReducer from '../reducers';
 
 const loggerMiddleware = createLogger();
 const middleWare = applyMiddleware(
@@ -13,12 +16,30 @@ const middleWare = applyMiddleware(
   loggerMiddleware
 );
 
-const reactotronEnhancer = createReactotronEnhancer(Reactotron, {
-  isActionImportant: action => action.type.match(/LOGIN/g),
-});
-const composition = compose(reactotronEnhancer, middleWare);
+// To purge the storage, set to true
+const PURGE_STORAGE = false;
 
-const configureStore = initialState =>
-  createStore(rootReducer, initialState, composition);
+
+const configureStore = (initialState) => {
+  let composition = compose(middleWare, autoRehydrate());
+
+  if (__DEV__) {
+    const reactotronEnhancer = createReactotronEnhancer(Reactotron, {
+      isActionImportant: action => action.type.match(/LOGIN/g),
+    });
+    composition = compose(reactotronEnhancer, composition);
+  }
+
+  const store = createStore(rootReducer, initialState, composition);
+
+  if (PURGE_STORAGE) {
+    persistStore(store).purge();
+  } else {
+    persistStore(store, {}, () => console.log('rehydration complete'));
+  }
+
+  return store;
+};
+
 
 export default configureStore;
