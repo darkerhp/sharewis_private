2b4c8c99774cccf69315f12b42018fbc58964255
norikt <norikt@gmail.com>
2016-09-13 12:51:22 +0900

    Fix lint

--- src/utils/course.js
+++ src/utils/course.js
@@ -1,11 +1,11 @@
 // @flow
-export const totalLectureCount = (course) =>
+export const totalLectureCount = course =>
   course.lectures.filter(l => l.kind === 'lecture').length;
 
-export const completeLectureCount = (course) =>
+export const completeLectureCount = course =>
   course.lectures.filter(l => l.isCompleted).length;
 
-export const totalDuration = (course) =>
+export const totalDuration = course =>
   course.lectures.map(l => l.duration || 0).reduce((a, b) => a + b);
 
 export const getNextLecture = (course) => {
