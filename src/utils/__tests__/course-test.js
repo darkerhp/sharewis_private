8ab1bc3e19b2053df6b654cecf84d1892f674ca1
norikt <norikt@gmail.com>
2016-09-13 12:38:16 +0900

    Add util test

---
+++ src/utils/__tests__/course-test.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-undef */
+/* eslint no-console: ["error", { allow: ["error", "log"] }] */
+import * as CourseUtil from '../course';
+
+// Test Data
+const course = {
+  title: '差がつくビジネス戦略講座 | 事業開発・Platform戦略(R)・ITマーケティング',
+  lectures: [
+    { title: 'セクション１', kind: 'section' },
+    {
+      order: 1,
+      title: 'レクチャーA',
+      kind: 'lecture',
+      duration: 30,
+      isCompleted: true,
+      type: 'VideoLecture',
+    },
+    {
+      order: 2,
+      title: 'レクチャーB',
+      kind: 'lecture',
+      duration: 60,
+      isCompleted: false,
+      type: 'TextLecture',
+    },
+    {
+      order: 3,
+      title: 'レクチャーC',
+      kind: 'lecture',
+      duration: 90,
+      isCompleted: false,
+      type: 'VideoLecture',
+    },
+  ],
+};
+
+describe('Course Utils', () => {
+  it('should return totalLectureCount', () =>
+    expect(CourseUtil.totalLectureCount(course)).toEqual(3)
+  );
+
+  it('should return completeLectureCount', () =>
+    expect(CourseUtil.completeLectureCount(course)).toEqual(1)
+  );
+
+  it('should return totalDuration', () =>
+    expect(CourseUtil.totalDuration(course)).toEqual(180)
+  );
+
+  it('should return next lecture', () => {
+    nextLecture = CourseUtil.getNextLecture(course);
+    expect(nextLecture.order).toEqual(3);
+  });
+});
