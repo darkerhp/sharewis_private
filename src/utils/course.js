0c0b3409ffa884558bc08519d50485476bf4af64
norikt <norikt@gmail.com>
2016-09-16 11:56:28 +0900

    refactor getNextLecture -> getNextVideoLecture and adding test

--- src/utils/course.js
+++ src/utils/course.js
@@ -8,14 +8,18 @@ export const completeLectureCount = course =>
 export const totalDuration = course =>
   course.lectures.map(l => l.duration || 0).reduce((a, b) => a + b);
 
-export const getNextLecture = (course) => {
+export const getNextVideoLecture = (course, ignoreCompleted = true) => {
   const filteredLecture = course.lectures
     .filter(l =>
-      l.kind === 'lecture' && l.type === 'VideoLecture' && l.isCompleted === false
+      l.kind === 'lecture' && l.type === 'VideoLecture' && l.isCompleted === !ignoreCompleted
     ).sort((a, b) => {
       if (a.order === b.order) return 0;
       return a.order < b.order ? -1 : 1;
     });
   return filteredLecture[0] || {};
+};
+
+export const getLectureById = (course, lectureId) => {
+  return course.lectures.filter(l => l.kind === 'lecture').find((lecture) => lecture.id === lectureId);
 };
 
