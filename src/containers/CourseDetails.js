4ad1269ac6baa6be82e12f0dd1e4384f97156350
norikt <norikt@gmail.com>
2016-09-17 17:32:52 +0900

    Rebase master

--- src/containers/CourseDetails.js
+++ src/containers/CourseDetails.js
@@ -7,6 +7,7 @@ import { Actions as RouterActions } from 'react-native-router-flux';
 import LectureList from '../components/CourseDetails/LectureList';
 import CourseInfoSection from '../components/CourseDetails/CourseInfoSection';
 import * as CourseUtils from '../utils/course';
+import * as LectureUtils from '../utils/lecture';
 import connectToProps from '../utils/redux';
 import BaseStyles from '../baseStyles';
 
@@ -16,10 +17,6 @@ const { Dimensions, ScrollView, StatusBar, View } = ReactNative;
 const { height } = Dimensions.get('window');
 const HALF_DISPLAY_HEIGHT = (height - BaseStyles.navbarHeight) / 2;
 const QUARTER_DISPLAY_HEIGHT = (height - BaseStyles.navbarHeight) / 4;
-
-const styles = StyleSheet.create({
-  container: { flex: 1, paddingTop: BaseStyles.navbarHeight },
-});
 
 class CourseDetails extends Component {
   static propTypes = {
@@ -37,19 +34,7 @@ class CourseDetails extends Component {
     // })).isRequired,
   };
 
-  // componentWillMount() { console.log('[CourseDetails] Component Will Mount', arguments); }
-  // componentDidMount() { console.log('[CourseDetails] Component Did Mount', arguments); }
-  // componentWillReceiveProps() {
-  //  console.log('[CourseDetails] Component Will Receive Props', arguments);
-  // }
-  // shouldComponentUpdate() { console.log('[CourseDetails] Should Component Update', arguments); }
-  // componentWillUpdate() { console.log('[CourseDetails] Component Will Update', arguments); }
-  // componentDidUpdate() { console.log('[CourseDetails] Component Did Update', arguments); }
-  // componentWillUnmount() { console.log('[CourseDetails] Component Will Unmount', arguments); }
-
   @autobind
-  // TODO ↓this.prop使うようになったら消す
-  // eslint-disable-next-line class-methods-use-this
   handlePressNextLecture() {
     const { course } = this.props;
     const nextLecture = CourseUtils.getNextLecture(course);
@@ -61,10 +46,8 @@ class CourseDetails extends Component {
   }
 
   @autobind
-  // TODO ↓this.prop使うようになったら消す
-  // eslint-disable-next-line class-methods-use-this
   handlePressLecture(lecture) {
-    // const { course } = this.props; TODO propを受け取る
+    const { course } = this.props;
     RouterActions.lecture({
       title: lecture.title,
       lectureId: lecture.id,
