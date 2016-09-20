cfff044466b2952f608755fe7f4112e11d70d08d
norikt <norikt@gmail.com>
2016-09-20 17:05:36 +0900

    Install react-native-fs

--- android/app/src/main/java/com/sharewis/ShareWisAct/MainApplication.java
+++ android/app/src/main/java/com/sharewis/ShareWisAct/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
 import com.brentvatne.react.ReactVideoPackage;
+import com.rnfs.RNFSPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -38,7 +39,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new ReactNativeI18n(),
           new ReactVideoPackage(),
-          new FBSDKPackage(mCallbackManager)
+          new FBSDKPackage(mCallbackManager),
+          new RNFSPackage()
       );
     }
   };
