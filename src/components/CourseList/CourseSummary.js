9513f47fd55925fd179041069e5c28691f180721
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-09-23 15:28:54 +0900

    refix vars from api

--- src/components/CourseList/CourseSummary.js
+++ src/components/CourseList/CourseSummary.js
@@ -69,9 +69,9 @@ const CourseSummary = ({ course, ...props }) =>
           <Hr lineColor={'#dadada'} />
         </View>
         <Text style={styles.progress}>
-          {`${course.nb_lectures_watched}/${course.total_nb_lectures} ${I18n.t('progressText')}`}
+          {`${course.lectureProgress}/${course.lectureCount} ${I18n.t('progressText')}`}
         </Text>
-        <ProgressBar progress={course.nb_lectures_watched / course.total_nb_lectures} />
+        <ProgressBar progress={course.lectureProgress / course.lectureCount} />
         <View style={styles.download}>
           <Text style={styles.downloadText}>
             {I18n.t('downloadAvailable')}
