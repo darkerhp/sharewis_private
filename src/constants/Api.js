1b06159c2e3f77fccd8cdd3a576f8adbf3598618
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-09-26 17:35:55 +0900

    add actApi middleware

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -3,16 +3,20 @@
 let ACCOUNT_API_HOST = 'account.share-wis.com';
 let ACCOUNT_API_BASE_PATH = '';
 let ACT_API_HOST = 'act.share-wis.com';
+let actApiKey = '?????????';  // TODO
 
 
 if (__DEV__) {
   ACCOUNT_API_HOST = 'dev-account.share-wis.com';
   ACCOUNT_API_BASE_PATH = '/dev';
   ACT_API_HOST = 'staging-act.share-wis.com';
+  actApiKey = 'Pr4BP2BedViT6Gjm2aiwRJAy6ziTs2i6yGrsyTp9';
 }
 
 
 export const ACCOUNT_API_URL = `https://${ACCOUNT_API_HOST}${ACCOUNT_API_BASE_PATH}`;
 export const ACT_API_URL = `https://${ACT_API_HOST}`;
+export const ACT_API_KEY = actApiKey;
+
 
 export const PASSWORD_FORGOTTEN_URL = `${ACT_API_URL}/users/password/new`;
---
+++ src/middleware/actApi.js
@@ -0,0 +1,29 @@
+/**
+ * @flow
+ */
+/* global fetch */
+import { ACT_API_URL, ACT_API_KEY } from '../constants/Api';
+import { checkStatus, checkResult } from '../utils/api';
+
+
+const getHeaders = userId => ({
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+  'app-api-key': ACT_API_KEY,
+  'user-id': userId,
+});
+
+
+const getUserCourses = async (userId) => {
+  // Run query
+  const result = await fetch(`${ACT_API_URL}/courses/me`, {
+    headers: getHeaders(userId),
+  });
+  // Verify results
+  await checkStatus(result);
+  const json = await result.json();
+  await checkResult(json, courses => courses.data);
+
+  // Parse and return results
+  return json.data;
+};
