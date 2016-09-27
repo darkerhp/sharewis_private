88e54952b185f079c0ff51bd64003db7194efb20
norikt <norikt@gmail.com>
2016-09-27 18:20:04 +0900

    bracket

--- src/utils/file.js
+++ src/utils/file.js
@@ -3,10 +3,10 @@ import RNFS from 'react-native-fs';
 
 const ARCHIVE_PATH = `${RNFS.DocumentDirectoryPath}/archive`;
 
-export const getCourseArchivePath = (courseId) =>
+export const getCourseArchivePath = courseId =>
   `${ARCHIVE_PATH}/${courseId}`;
 
-export const getCourseVideosDirPath = (courseId) =>
+export const getCourseVideosDirPath = courseId =>
   `${getCourseArchivePath(courseId)}/videos`;
 
 // ARCHIVE_PATH/archive/{course_id}/videos/{lecture_id}.mp4
