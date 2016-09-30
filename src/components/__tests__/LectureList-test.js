97b7611ce6be091d484d2135c237300c21ed3c3f
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-09-30 16:53:48 +0900

    snapshot LectureList

---
+++ src/components/__tests__/LectureList-test.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+import 'react-native';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import LectureList from '../CourseDetails/LectureList';
+
+jest.mock('./LectureItem', () => 'LectureItem');
+
+describe('LectureList', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <LectureList
+        courseId={0}
+        lectures={[]}
+        containerStyleId={0}
+        handlePressDelete={() => null}
+        handlePressDownload={() => null}
+        handlePressLecture={() => null}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
