be71e5782a4e682486a9a62a5075482b0ac94fe4
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-09-30 15:04:00 +0900

    add snapshot test for Slide

---
+++ src/components/__tests__/Onboarding-tests.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+import 'react-native';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import Slide from '../Onboarding/Slide';
+
+
+describe('Onboarding', () => {
+  describe('Slide', () => {
+    it('renders correctly', () => {
+      const tree = renderer.create(
+        <Slide />
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+});
