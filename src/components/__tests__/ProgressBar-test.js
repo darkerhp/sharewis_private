813c7aa698a75e66516d9b0d0998cc00b7034f81
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-09-30 16:34:01 +0900

    snapshot ProgressBar

---
+++ src/components/__tests__/ProgressBar-test.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+import 'react-native';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import ProgressBar from '../ProgressBar';
+
+
+describe('ProgressBar', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <ProgressBar
+        progress={20}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
