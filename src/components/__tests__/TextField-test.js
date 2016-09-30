eaf41d42f6ab54dbabd9fbb2d4fb43877f666082
Adrien Lemaire <lemaire.adrien@gmail.com>
2016-09-30 16:36:17 +0900

    snapshot TextField

---
+++ src/components/__tests__/TextField-test.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+import 'react-native';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import TextField from '../TextField';
+
+
+describe('TextField', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <TextField
+        input={{
+          value: 'text',
+          onChange: () => null,
+        }}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
