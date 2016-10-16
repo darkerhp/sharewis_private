8491bcf59fb2a9d627d846f2e28695158fdc1b6f
norikt <norikt@gmail.com>
2016-10-16 16:46:12 +0900

    翻訳テキスト内のリンクURLを修正

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -1,12 +1,12 @@
 import I18n from 'react-native-i18n';
 import rawTranslations from '../data/translations.json';
-import { ACT_API_URL } from '../constants/Api';
+import { ACT_SITE_URL } from '../constants/Api';
 
 
 export default function setupI18n() {
   // Set localization
   let stringTranslations = JSON.stringify(rawTranslations);
-  stringTranslations = stringTranslations.replace(/ACT_API_URL/gm, ACT_API_URL);
+  stringTranslations = stringTranslations.replace(/ACT_SITE_URL/gm, ACT_SITE_URL);
   I18n.fallbacks = true;
   I18n.defaultLocale = 'en-US';
   I18n.translations = JSON.parse(stringTranslations);
