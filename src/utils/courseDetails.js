72d1c5905789feec61f1e3fcb3bc1595c0846633
norikt <norikt@gmail.com>
2016-10-17 18:08:48 +0900

    Remove unused files

--- src/utils/courseDetails.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import _ from 'lodash';
-
-const totalDuration = (lectures) => {
-  if (_.isEmpty(lectures)) return 0;
-  return _.reduce(lectures, (result, value, key) => result + (value.estimatedTime || 0), 0);
-};
-
-export default totalDuration;
