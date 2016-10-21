16863b76585386da89b047114833b19fd834e683
norikt <norikt@gmail.com>
2016-10-21 17:59:57 +0900

    Add test

---
+++ src/components/__tests__/OfflineLecutre-test.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/no-extraneous-dependencies */
+import 'react-native';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import OfflineLecture from '../Lecture/OfflineLecture';
+
+describe('OfflineLecture', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <OfflineLecture lectureContentStyleId={1} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
