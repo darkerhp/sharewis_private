819fec417ab10452a2d4944e182dc9056444dc2b
norikt <norikt@gmail.com>
2016-10-28 17:46:28 +0900

    アカウント設定ページのたたき台を作成

--- src/containers/Account.js
+++ src/containers/Account.js
@@ -1,37 +1,132 @@
 import React from 'react';
-import {View, Text, StyleSheet} from "react-native";
-import Button from "react-native-button";
-import {Actions} from "react-native-router-flux";
+import ReactNative from 'react-native';
 
+import Button from 'react-native-button';
+import { Actions } from 'react-native-router-flux';
+
+import I18n from 'react-native-i18n';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import TextField from '../components/TextField';
+
+import BaseStyles from '../baseStyles';
+
+const { Component, PropTypes } = React;
+const { View, Text, StyleSheet, Platform, TextInput, Dimensions } = ReactNative;
+const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: BaseStyles.onboardingBackgroundColor,
+    paddingTop: 90,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
+  profileImageContainer: {
+    flex: 2,
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
+  profileContainer: {
+    flex: 2,
+    width,
+  },
+  fbContainer: {
+    flex: 1,
+  },
+  notificationContainer: {
+    flex: 1,
+  },
+  labelWrapper: {
+    alignItems: 'stretch',
+    marginHorizontal: 13,
+    marginBottom: 4,
+  },
+  label: {
+    color: '#666',
+    fontSize: 13,
+  },
+  profileContentWrapper: {
+    backgroundColor: 'white',
+    borderColor: '#dadada',
+    borderWidth: 1,
+    paddingLeft: 13,
+  },
+  rowWrapper: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderColor: '#dadada',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+  },
+  fieldNameWrapper: {
+    flex: 1,
+  },
+  fieldValueWrapper: {
+    flex: 2,
+  },
+  innerTextInput: {
+    ...Platform.select({
+      ios: {
+        borderLeftWidth: 0,
+        borderBottomWidth: 1,
+      },
+    }),
   },
 });
 
+@connect(({ user }) => ({ user }))
+class Account extends Component {// eslint-disable-line
+  static propTypes = {
+    user: PropTypes.shape({}).isRequired,
+  };
 
-export default class extends React.Component {
-  render(){
+  render() {
+
+    const { user } = this.props;
+
     return (
-      <View style={[styles.container, this.props.style]}>
-        <Text>Login page: {this.props.data}</Text>
-        <Button onPress={Actions.loginModal2}>Login 2</Button>
-        <Button onPress={() => Actions.refresh({title:"Changed title"})}>Change title</Button>
-        <Button onPress={Actions.pop}>Back</Button>
+      <View style={styles.container}>
+        {/* <View style={styles.profileImageContainer} /> */}
+
+        <View style={styles.profileContainer}>
+          <View style={styles.labelWrapper}>
+            <Text style={styles.label}>
+              プロフィール
+            </Text>
+          </View>
+          <View style={styles.profileContentWrapper}>
+            <View style={styles.rowWrapper}>
+              <View style={styles.fieldNameWrapper}>
+                <Text>ユーザー名</Text>
+              </View>
+              <View style={styles.fieldValueWrapper}>
+                <Text>{user.userName}</Text>
+              </View>
+            </View>
+            <View style={styles.rowWrapper}>
+              <View style={styles.fieldNameWrapper}>
+                <Text>ニックネーム</Text>
+              </View>
+              <View style={styles.fieldValueWrapper}>
+                <Text>{user.nickName}</Text>
+              </View>
+            </View>
+            <View style={[styles.rowWrapper, { borderBottomWidth: 0 }]}>
+              <View style={styles.fieldNameWrapper}>
+                <Text>メールアドレス</Text>
+              </View>
+              <View style={styles.fieldValueWrapper}>
+                <Text>{user.email}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.fbContainer} />
+        <View style={styles.notificationContainer} />
       </View>
     );
   }
 }
+
+export default Account;
