e20f2cc4f3abc1637790506d82b1ee7b2d3c0aab
norikt <norikt@gmail.com>
2016-11-08 10:59:04 +0900

    Add alert util

---
+++ src/utils/alert.js
@@ -0,0 +1,14 @@
+// @flow
+import ReactNative from 'react-native';
+import I18n from 'react-native-i18n';
+
+const { Alert } = ReactNative;
+
+const alertOfflineError = () =>
+  Alert.alert(
+    I18n.t('offlineErrorTitle'),
+    I18n.t('offlineErrorMessage'),
+    [{ text: I18n.t('close') }],
+  );
+
+export default alertOfflineError;
