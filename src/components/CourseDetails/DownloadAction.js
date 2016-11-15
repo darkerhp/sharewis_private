c2a26e10e2d80bb45ee48620ef67e37f31fc48a3
norikt <norikt@gmail.com>
2016-11-15 16:35:36 +0900

    Remove unused

--- src/components/CourseDetails/DownloadAction.js
+++ src/components/CourseDetails/DownloadAction.js
@@ -6,97 +6,10 @@ import * as Progress from 'react-native-progress';
 import BaseStyles from '../../baseStyles';
 
 const { PropTypes } = React;
-const { Platform, StyleSheet, TouchableOpacity } = ReactNative;
+const { StyleSheet, TouchableOpacity } = ReactNative;
 
 const lectureRowHeight = 48;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-    borderColor: BaseStyles.borderColor,
-    borderBottomWidth: 1,
-  },
-  lectureDisabled: {
-    ...Platform.select({
-      android: {
-        opacity: 0.2,
-      },
-      ios: {
-        opacity: 0.4,
-      },
-    }),
-  },
-  lectureNoTextWrapper: {
-    flex: 1,
-    height: lectureRowHeight,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: BaseStyles.borderColor,
-    borderRightWidth: 1,
-  },
-  lectureNoTextWrapperCompleted: { // FIXME lectureNoTextWrapperと共通化できる？
-    flex: 1,
-    height: lectureRowHeight,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: BaseStyles.backgroundColor,
-    borderRightWidth: 1,
-    backgroundColor: BaseStyles.backgroundColor,
-  },
-  lectureNoText: {
-    fontSize: 14,
-    color: BaseStyles.textColor,
-    fontWeight: '600',
-  },
-  lectureNoTextCompleted: {
-    fontSize: 14,
-    color: 'white',
-    fontWeight: '600',
-  },
-  lectureInfoWrapper: {
-    flex: 1,
-    height: lectureRowHeight,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  lectureIconWrapper: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  lectureIcon: {
-    color: BaseStyles.textColor,
-    fontSize: 15,
-  },
-  durationWrapper: {
-    flex: 1,
-  },
-  durationStyle: {
-    fontSize: 8,
-    padding: 3,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#f2f2f2',
-    ...Platform.select({
-      android: {
-        color: BaseStyles.textColor,
-        width: 30,  // or seconds will be trimmed in android
-      },
-    }),
-  },
-  lectureTitleTextWrapper: {
-    flex: 5,
-    height: lectureRowHeight,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-  },
-  lectureTitleText: {
-    fontSize: 12,
-    color: BaseStyles.textColor,
-    fontWeight: 'bold',
-  },
   actionIconWrapper: {
     flex: 1,
     height: lectureRowHeight,
