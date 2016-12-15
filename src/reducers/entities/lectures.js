25bc6935eff74191df342176a1710ce9e5ab411e
norikt <norikt@gmail.com>
2016-12-15 17:54:09 +0900

    Lecture reducers to Immutable

--- src/reducers/entities/lectures.js
+++ src/reducers/entities/lectures.js
@@ -2,110 +2,106 @@
 import normalize from 'normalize-object';
 import _ from 'lodash';
 import { handleActions } from 'redux-actions';
+import { fromJS } from 'immutable';
+
 import { LECTURE_STATUS_FINISHED } from '../../constants/Api';
 
-const lecturesReducer = handleActions({
-  FETCH_COURSE_DETAILS_SUCCESS: (state, action) => ({
-    ...state,
-    ...normalize(action.payload.lectures.entities.lectures), // プロパティをキャメルケースに変換
-  }),
-  COMPLETE_LECTURE: (state, action) => {
-    const lectureId = action.payload;
-    const targetLecture = state[lectureId];
+import Lecture from '../../models/Lecture';
+import LectureMap from '../../models/LectureMap';
 
-    return {
-      ...state,
-      [lectureId]: {
-        ...targetLecture,
-        status: LECTURE_STATUS_FINISHED,
-      },
-    };
-  },
-  BEGIN_DOWNLOAD_VIDEO: (state, action) => {
-    const { lectureId, jobId } = action.payload;
-    return {
-      ...state,
-      [lectureId]: {
-        ...state[lectureId],
-        isDownloading: true,
-        jobId,
-      },
-    };
-  },
-  PROGRESS_DOWNLOAD_VIDEO: (state, action) => {
-    const { lectureId, jobId, progress } = action.payload;
-    const lecture = state[lectureId];
-    if (lecture.jobId === -1) return state;
-    return {
-      ...state,
-      [lectureId]: {
-        ...state[lectureId],
-        isDownloading: true,
-        jobId,
-        progress,
-      },
-    };
-  },
-  FINISH_DOWNLOAD_VIDEO: (state, action) => {
-    const lectureId = action.payload;
-    return {
-      ...state,
-      [lectureId]: {
-        ...state[lectureId],
-        hasVideoInDevice: true,
-        isDownloading: false,
-        jobId: -1,
-        progress: 0,
-      },
-    };
-  },
-  ERROR_DOWNLOAD_VIDEO: (state, action) => {
-    const lectureId = action.payload;
-    return {
-      ...state,
-      [lectureId]: {
-        ...state[lectureId],
-        hasVideoInDevice: false,
-        isDownloading: false,
-        jobId: -1,
-        progress: 0,
-      },
-    };
-  },
-  CANCEL_DOWNLOAD_VIDEO: (state, action) => {
-    const lectureId = action.payload;
-    return {
-      ...state,
-      [lectureId]: {
-        ...state[lectureId],
-        isDownloading: false,
-        jobId: -1,
-      },
-    };
-  },
-  FINISH_DELETE_VIDEO: (state, action) => {
-    const lectureId = action.payload;
-    return {
-      ...state,
-      [lectureId]: {
-        ...state[lectureId],
-        hasVideoInDevice: false,
-        progress: 0,
-      },
-    };
+const initialState = new LectureMap();
+
+const mergeEntities = (state, newLectures) =>
+  state.merge(newLectures.map(lecture => new Lecture(lecture)));
+
+const lecturesReducer = handleActions({
+  // APIから取得したデータをstateに設定する
+  FETCH_COURSE_DETAILS_SUCCESS: (state, action) => {
+    const lectures = action.payload.lectures.entities.lectures;
+    if (!lectures) return state;
+    return mergeEntities(state, fromJS(normalize(lectures)));
   },
   UPDATE_VIDEO_IN_DEVICE_STATUS: (state, action) => {
     if (_.isEmpty(state)) return state;
     const lectures = action.payload;
-    let newLectures = { ...state }; // eslint-disable-line
-    lectures.forEach(l =>
-      (newLectures[l.lectureId].hasVideoInDevice = l.hasVideoInDevice),
-    );
-    return {
-      ...state,
-      ...newLectures,
-    };
+    return mergeEntities(state, fromJS(lectures));
   },
-}, {});
+  COMPLETE_LECTURE: (state, action) => {
+    const lectureId = action.payload;
+    return state.update(lectureId, lecture => lecture.set('status', LECTURE_STATUS_FINISHED));
+  },
+  BEGIN_DOWNLOAD_VIDEO: (state, action) => {
+    const { lectureId, jobId } = action.payload;
+    return state.update(lectureId,
+      (lecture) => {
+        lecture.merge({
+          isDownloading: true,
+          jobId,
+        });
+      },
+    );
+  },
+  PROGRESS_DOWNLOAD_VIDEO: (state, action) => {
+    const { lectureId, jobId, progress } = action.payload;
+    if (state[lectureId].jobId === -1) return state;
+    return state.update(lectureId,
+      (lecture) => {
+        lecture.merge({
+          isDownloading: true,
+          jobId,
+          progress,
+        });
+      },
+    );
+  },
+  FINISH_DOWNLOAD_VIDEO: (state, action) => {
+    const lectureId = action.payload;
+    return state.update(lectureId,
+      (lecture) => {
+        lecture.merge({
+          hasVideoInDevice: true,
+          isDownloading: false,
+          jobId: -1,
+          progress: 0,
+        });
+      },
+    );
+  },
+  ERROR_DOWNLOAD_VIDEO: (state, action) => {
+    const lectureId = action.payload;
+    return state.update(lectureId,
+      (lecture) => {
+        lecture.merge({
+          hasVideoInDevice: false,
+          isDownloading: false,
+          jobId: -1,
+          progress: 0,
+        });
+      },
+    );
+  },
+  CANCEL_DOWNLOAD_VIDEO: (state, action) => {
+    const lectureId = action.payload;
+    return state.update(lectureId,
+      (lecture) => {
+        lecture.merge({
+          isDownloading: false,
+          jobId: -1,
+        });
+      },
+    );
+  },
+  FINISH_DELETE_VIDEO: (state, action) => {
+    const lectureId = action.payload;
+    return state.update(lectureId,
+      (lecture) => {
+        lecture.merge({
+          hasVideoInDevice: false,
+          progress: 0,
+        });
+      },
+    );
+  },
+}, initialState);
 
 export default lecturesReducer;
