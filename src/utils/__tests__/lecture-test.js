50055a756b500c7c89a201ad7b3ae2748b91c791
norikt <norikt@gmail.com>
2016-12-16 14:25:21 +0900

    Convert to immutable js

--- src/utils/__tests__/lecture-test.js
+++ src/utils/__tests__/lecture-test.js
@@ -3,6 +3,8 @@ import * as ApiConstants from '../../constants/Api';
 import * as LectureUtils from '../lecture';
 import { lectures } from '../../data/dummyData';
 
+import Lecture from '../../models/Lecture';
+import LectureMap from '../../models/LectureMap';
 
 describe('LectureUtils Utils', () => {
   describe('getLectureIconName', () => {
@@ -39,10 +41,20 @@ describe('LectureUtils Utils', () => {
   });
 
   it('should return next video lecture', () => {
-    nextLecture = LectureUtils.getNextVideoLecture(180, lectures);
+    const lectureMap = new LectureMap(lectures).map(lecture => new Lecture(lecture));
+
+    let nextLecture = LectureUtils.getNextVideoLecture(180, lectureMap);
     expect(nextLecture.order).toEqual(5);
 
-    nextLecture = LectureUtils.getNextVideoLecture(180, lectures, false);
+    nextLecture = LectureUtils.getNextVideoLecture(180, lectureMap, false);
     expect(nextLecture.order).toEqual(1);
   });
+
+  it('should return last lectureId', () => {
+    const lectureMap = new LectureMap(lectures).map(lecture => new Lecture(lecture));
+
+    const lastLectureId = LectureUtils.getLastLectureId(180, lectureMap);
+    expect(lastLectureId).toEqual(5930);
+  });
+
 });
--- src/utils/lecture.js
+++ src/utils/lecture.js
@@ -1,47 +1,45 @@
 import _ from 'lodash';
-import * as ApiConstants from '../constants/Api';
+import * as Const from '../constants/Api';
 
 // @flow
 export const getLectureIconName = (lecture) => {
   switch (lecture.type) {
-    case ApiConstants.LECTURE_TYPE_VIDEO:
+    case Const.LECTURE_TYPE_VIDEO:
       return 'play-circle-filled';
-    case ApiConstants.LECTURE_TYPE_TEXT:
+    case Const.LECTURE_TYPE_TEXT:
       return 'text-format';
-    case ApiConstants.LECTURE_TYPE_PDF:
+    case Const.LECTURE_TYPE_PDF:
       return 'picture-as-pdf';
-    case ApiConstants.LECTURE_TYPE_AUDIO:
+    case Const.LECTURE_TYPE_AUDIO:
       return 'audiotrack';
-    case ApiConstants.LECTURE_TYPE_QUIZ:
+    case Const.LECTURE_TYPE_QUIZ:
       return 'question-answer';
-    case ApiConstants.LECTURE_TYPE_ATTACHMENT:
-      return ApiConstants.LECTURE_TYPE_ATTACHMENT;
+    case Const.LECTURE_TYPE_ATTACHMENT:
+      return Const.LECTURE_TYPE_ATTACHMENT;
     default:
       return 'error';  // TODO
   }
 };
 
 export const getNextVideoLecture = (courseId, lectures, skipCompleted = true, currentOrder = 0) => {
-  let videoLectures = _.filter(lectures, lecture => (
-    lecture.courseId === courseId &&
-    lecture.kind === ApiConstants.LECTURE_KIND_LECTURE &&
-    lecture.type === ApiConstants.LECTURE_TYPE_VIDEO && // TODO enable other lecture types
-    lecture.order > currentOrder
+  let videoLectures = lectures.filter(l => (
+    l.courseId === courseId &&
+    l.kind === Const.LECTURE_KIND_LECTURE &&
+    (l.type === Const.LECTURE_TYPE_VIDEO || l.type === Const.LECTURE_TYPE_TEXT) &&
+    l.order > currentOrder
   ));
-
   if (skipCompleted) {
-    videoLectures = _.reject(videoLectures, { status: ApiConstants.LECTURE_STATUS_FINISHED });
+    videoLectures = videoLectures.filterNot(l => l.status === Const.LECTURE_STATUS_FINISHED);
   }
-
-  return _.sortBy(videoLectures, ['order'])[0] || {};
+  return videoLectures.sortBy(l => l.order).first();
 };
 
 export const getLastLectureId = (courseId, lectures) => {
-  const videoLectures = _.filter(lectures, lecture => (
-    lecture.courseId === courseId &&
-    lecture.kind === ApiConstants.LECTURE_KIND_LECTURE &&
-    lecture.type === ApiConstants.LECTURE_TYPE_VIDEO // TODO enable other lecture types
+  const videoLectures = lectures.filter(l => (
+    l.courseId === courseId &&
+    l.kind === Const.LECTURE_KIND_LECTURE &&
+    (l.type === Const.LECTURE_TYPE_VIDEO || l.type === Const.LECTURE_TYPE_TEXT)
   ));
 
-  return _.sortBy(videoLectures, ['order'])[videoLectures.length - 1].id;
+  return videoLectures.sortBy(l => l.order).last().id;
 };
