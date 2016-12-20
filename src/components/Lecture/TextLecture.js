6a265eff7aa771d06ef85e5a01776955d7f6f3c2
norikt <norikt@gmail.com>
2016-12-20 15:51:05 +0900

    Implement TextLecture Component

--- src/components/Lecture/TextLecture.js
+++ src/components/Lecture/TextLecture.js
@@ -1,201 +1,54 @@
 import React, { Component, PropTypes } from 'react';
 import ReactNative from 'react-native';
 
-import autobind from 'autobind-decorator';
-import Video from 'react-native-video';
-import { Actions as RouterActions } from 'react-native-router-flux';
-import Orientation from 'react-native-orientation';
-
-import VideoControls from './VideoControls';
-import FullScreenVideoControls from './FullScreenVideoControls';
 import Lecture from '../../models/Lecture';
+import BaseStyles from '../../baseStyles';
 
 const {
-  ActivityIndicator,
-  Image,
-  StatusBar,
   StyleSheet,
+  Text,
   View,
+  WebView,
 } = ReactNative;
 
 const styles = StyleSheet.create({
-  backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    backgroundColor: 'white',
-  },
-  videoContainer: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-    marginTop: 64,
-  },
-  loadingIndicatorWrapper: {
+  webViewWrapper: { flex: 9 },
+  webView: {},
+  lectureTitleTextWrapper: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lectureTitle: {
+    fontSize: 17,
+    color: '#e0e0e0',
+    fontWeight: 'bold',
   },
 });
 
-class VideoLecture extends Component {
-  static propTypes = {
-    // values
-    currentLecture: PropTypes.instanceOf(Lecture).isRequired,
-    currentTime: PropTypes.number.isRequired,
-    isFullScreen: PropTypes.bool.isRequired,
-    isPaused: PropTypes.bool.isRequired,
-    isStarted: PropTypes.bool.isRequired,
-    lectureContentStyleId: PropTypes.number.isRequired,
-    speed: PropTypes.number.isRequired,
-    // actions
-    changeVideoPlaySpeed: PropTypes.func.isRequired,
-    toggleFullScreen: PropTypes.func.isRequired,
-    togglePlay: PropTypes.func.isRequired,
-    updateVideoProgress: PropTypes.func.isRequired,
-  };
+const TextLecture = ({ currentLecture, lectureContentStyleId }) => {
 
-  static toFullScreen() {
-    Orientation.lockToLandscape();
-    StatusBar.setHidden(true);
-    RouterActions.refresh({ hideNavBar: true });
-  }
+  // TODO DEBUG
+  currentLecture = currentLecture.set('body', '<p>このコースで解説した内容をまとめました。<br>是非、これらのコミュニケーション術を身につけて、相手との会話をはずませましょう！</p><p><br></p><h3>話し方のポイント</h3><p>職場の男性やご主人とお話していて『要するに何が言いたいの』とか『話がわかりにくい-』と言われたことないですか？</p><p>男性にとっては一生懸命聞こうと思えば思うほど、きつい言葉の反応になりがちです。<br><span style="line-height: 1.45em;">もともと男性と女性の脳の構造の違いからコミュニケーションの仕方に差がでてくるんで-すね。</span></p><p>そんなコミュニケーション、ちょっと話す順番を変えてみるのはいかがでしょうか？</p><p>話し方のポイントとして、<b><font color="#fc0c59">結論からお話をしてみる</font></b>のも１つの方法です。</p><p>普段と話す順番をかえることで上手に相手に伝えられることがあります。</p><p><br></p><h3>会話のはひふへほ</h3><p>会話は、自分が話しているだけじゃないですよね。<br><p><span style="line-height: 1.45em;">相手の話を聞いて、相手も聞いてもらっている実感があると、さらに会話がはずみます。</span></p></p><p>会話がはずむコツ、「会話のはひふへほ」を試してみましょう。</p><p><br></p><p><b><font color="#fc0c59">は</font></b>ぁ～なるほど！</p><p><b><font color="#fc0c59">ふ</font></b>ーんそうなんだ～</p><p><b><font color="#fc0c59">へ</font></b>ぇーすごいねー！</p><p><b><font color="#fc0c59">ほ</font></b>ぉ～それでそれで！</p><p><br></p><h3>「すもも」でアドバイス</h3><p>ほめて伸ばそう！</p><p>口うるさくアドバイスしちゃう時ってないですか？</p><p>ほめ方って具体的にどうしたらいいかよくわからないって事はありませんか？</p><p>そんな時は、次の「すもも」をキーワードに褒めてアドバイスをしてみましょう。</p><p><br></p><p><b><font color="#fc0c59">す</font></b>ごく、いいね！</p><p><b><font color="#fc0c59">も</font></b>ったいないのは…</p><p><b><font color="#fc0c59">も</font></b>のすごくよかったのはこれだね！</p>');
 
-  static toPortrait() {
-    Orientation.lockToPortrait();
-    StatusBar.setHidden(false);
-    RouterActions.refresh({ hideNavBar: false });
-  }
-
-  state = {
-    seeking: false,
-    isLoadingThumbnail: true,
-  };
-
-  @autobind
-  handleValueChange() {
-    this.setState({ seeking: true }); // 再生位置調整中はビデオを止める
-  }
-
-  @autobind
-  handleSlidingComplete(value) {
-    this.setState({ seeking: false });
-    if (this.video) {
-      this.video.seek(value);
-    }
-  }
-
-  // 250ms毎に呼び出される
-  @autobind
-  handleVideoProgress(data) {
-    const { currentTime, updateVideoProgress } = this.props;
-    if (currentTime === data.currentTime) return;
-    // TODO パフォーマンスに問題があるため、actionの呼び出し回数を減らしたほうが良さそう
-    updateVideoProgress(data.currentTime);
-  }
-
-  @autobind
-  handlePressFullScreen() {
-    const { isFullScreen, toggleFullScreen } = this.props;
-    if (isFullScreen) {
-      VideoLecture.toPortrait();
-    } else {
-      VideoLecture.toFullScreen();
-    }
-    toggleFullScreen();
-  }
-
-
-  @autobind
-  renderVideo() {
-    const { currentLecture, isStarted, isPaused, speed } = this.props;
-    if (!isStarted) {
-      return (
-        <Image
-          style={styles.backgroundVideo}
-          source={{ uri: currentLecture.thumbnailUrl }}
-          onLoadStart={() => this.setState({ isLoadingThumbnail: true })}
-          onLoadEnd={() => this.setState({ isLoadingThumbnail: false })}
-          resizeMode="contain"
+  return (
+    <View style={[lectureContentStyleId, { marginTop: BaseStyles.navbarHeight }]}>
+      <View style={styles.lectureTitleTextWrapper}>
+        <Text style={styles.lectureTitle}>{currentLecture.title}</Text>
+      </View>
+      <View style={styles.webViewWrapper}>
+        <WebView
+          style={styles.webView}
+          source={{ html: currentLecture.body }}
         />
-      );
-    }
-
-    return (
-      <Video
-        muted={false}
-        onLoadStart={() => this.setState({ isLoadingThumbnail: true })}
-        onLoad={() => this.setState({ isLoadingThumbnail: false })}
-        onEnd={this.handlePressNextLecture}
-        onError={e => console.error(e)}
-        onProgress={this.handleVideoProgress}
-        paused={this.state.seeking || isPaused}
-        playInBackground={false}
-        playWhenInactive={false}
-        rate={speed}
-        ref={ref => (this.video = ref)}
-        repeat={false}
-        resizeMode="contain"
-        source={{ uri: currentLecture.getVideoUrl() }}
-        style={styles.backgroundVideo}
-        volume={1.0}
-      />
-    );
-  }
-
-  @autobind
-  renderIndicator() {
-    if (!this.state.isLoadingThumbnail) return null;
-    return (
-      <View style={styles.loadingIndicatorWrapper}>
-        <ActivityIndicator />
       </View>
-    );
-  }
+    </View>
+  );
+};
 
-  render() {
-    const {
-      currentTime,
-      currentLecture,
-      isFullScreen,
-      isPaused,
-      lectureContentStyleId,
-      speed,
-      changeVideoPlaySpeed,
-      togglePlay,
-    } = this.props;
+TextLecture.propTypes = {
+  currentLecture: PropTypes.instanceOf(Lecture).isRequired,
+  lectureContentStyleId: PropTypes.number.isRequired,
+};
 
-    const videoControlsProps = {
-      currentTime,
-      estimatedTime: currentLecture.estimatedTime,
-      isFullScreen,
-      isLoadingThumbnail: this.state.isLoadingThumbnail,
-      isPaused,
-      onPressFullScreen: this.handlePressFullScreen,
-      onPressPlay: togglePlay,
-      onPressSpeed: changeVideoPlaySpeed,
-      onSlidingComplete: this.handleSlidingComplete,
-      onValueChange: this.handleValueChange,
-      speed,
-      title: currentLecture.title,
-    };
-
-    return (
-      <View style={[lectureContentStyleId, isFullScreen && { flex: 1 }]}>
-        <View style={(isFullScreen ? { flex: 1 } : styles.videoContainer)}>
-          {this.renderVideo()}
-          {this.renderIndicator()}
-        </View>
-
-        {isFullScreen
-          ? <FullScreenVideoControls {...videoControlsProps} />
-          : <VideoControls {...videoControlsProps} />}
-      </View>
-    );
-  }
-}
-
-export default VideoLecture;
+export default TextLecture;
