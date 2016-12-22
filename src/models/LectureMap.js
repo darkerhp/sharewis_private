f995c7bb45845eaa1c0e173d304b8ee2d485f42e
norikt <norikt@gmail.com>
2016-12-22 12:31:44 +0900

    Move methods

--- src/models/LectureMap.js
+++ src/models/LectureMap.js
@@ -1,5 +1,31 @@
-import { OrderedMap } from 'immutable';
+import { OrderedMap } from 'extendable-immutable';
 
-const LectureMap = OrderedMap;
+export default class LectureMap extends OrderedMap {
+  static isLectureMap(value) {
+    return value && value instanceof LectureMap;
+  }
 
-export default LectureMap;
+  getNextLecture(courseId, skipCompleted = true, currentOrder = 0) {
+    let filteredLectures = this.filter(l => (
+      l.courseId === courseId &&
+      l.isLecture() &&
+      (l.isVideo() || l.isText()) && // TODO 今のところ動画とテキストだけ
+      l.order > currentOrder
+    ));
+    if (skipCompleted) {
+      filteredLectures = filteredLectures.filterNot(l => l.isFinished());
+    }
+    return filteredLectures.sortBy(l => l.order).first() || null;
+  }
+
+  getLastLectureId(courseId) {
+    const videoLectures = this.filter(l => (
+      l.courseId === courseId &&
+      l.isLecture() &&
+      (l.isVideo() || l.isText()) // TODO 今のところ動画とテキストだけ
+    ));
+
+    return videoLectures.sortBy(l => l.order).last().id;
+  };
+
+}
