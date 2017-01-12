b0a9063176388412e535893132b357cc501f8678
norikt <norikt@gmail.com>
2017-01-12 21:06:48 +0900

    Fix design

--- src/components/ScrollableTabs.js
+++ src/components/ScrollableTabs.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StatusBar, Text } from 'react-native';
 
-import ScrollableTabView, { ScrollableTabBar } from 'react-native-scrollable-tab-view';
+import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-view';
 
 import BaseStyles from '../baseStyles';
 import CourseList from '../containers/CourseList';
 
+// TODO プロコースが増えた場合、DefaultTabBar > ScrollableTabBarで実装する。
+// デザイン妥協できない場合、CustomでScrollableTabBarを実装する必要がありそう。
+const renderScrollableTabBar = () =>
+  <DefaultTabBar
+    underlineStyle={{ backgroundColor: '#ffff77', height: 2 }}
+    activeTextColor="#ffffff"
+    inactiveTextColor="#ffffff"
+    backgroundColor={BaseStyles.navBarBackgroundColor}
+  />;
 
-const ScrollableTabs = () =>
-  <ScrollableTabView
-    style={{marginTop: BaseStyles.navbarHeight, }}
-    initialPage={1} // 0が開始位置
-    renderTabBar={() => <ScrollableTabBar />}
-  >
-    <CourseList tabLabel='マイコース' />
-    <Text tabLabel='Top'>Top</Text>
-    <Text tabLabel='スナックコース'>スナックコース</Text>
-  </ScrollableTabView>;
+const ScrollableTabs = () => {
+  StatusBar.setBarStyle('light-content');
+
+  return (
+    <ScrollableTabView
+      style={{ marginTop: BaseStyles.navbarHeight - 1 }}
+      initialPage={1} // 0が開始位置
+      renderTabBar={() => renderScrollableTabBar()}
+    >
+      <CourseList tabLabel="マイコース" />
+      <Text tabLabel="Top">Top</Text>
+      <Text tabLabel="スナックコース">スナックコース</Text>
+    </ScrollableTabView>
+  );
+};
 
 export default ScrollableTabs;
