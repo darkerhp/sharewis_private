ebe3d94adff0b0cf3d2597bd5945cf469d407941
norikt <norikt@gmail.com>
2017-01-21 12:36:57 +0900

    Create component for one column course list

---
+++ src/components/CourseList/OneColumnItemBox.js
@@ -0,0 +1,54 @@
+import React, { PropTypes } from 'react';
+import ReactNative from 'react-native';
+
+import BaseStyles from '../../baseStyles';
+
+const {
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} = ReactNative;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: Dimensions.get('window').height / 2,
+    borderWidth: 1,
+    borderColor: BaseStyles.borderColor,
+    borderRadius: 9,
+    backgroundColor: 'white',
+    marginBottom: 13,
+    marginHorizontal: 10,
+    overflow: 'hidden',
+  },
+});
+
+const OneColumnItemBox = ({ style, isTouchble, ...props }) => {
+  if (isTouchble) {
+    return (
+      <TouchableOpacity
+        style={[styles.container, style]}
+        {...props}
+      />
+    );
+  }
+
+  return (
+    <View
+      style={[styles.container, style]}
+      {...props}
+    />
+  );
+};
+
+OneColumnItemBox.propTypes = {
+  style: PropTypes.any.isRequired, // eslint-disable-line
+  isTouchble: PropTypes.bool.isRequired,
+};
+
+OneColumnItemBox.defaultProps = {
+  isTouchble: true,
+};
+
+export default OneColumnItemBox;
