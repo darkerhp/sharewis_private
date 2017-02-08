139dafd891cd0a085977715c89c51b667365e5b3
norikt <norikt@gmail.com>
2017-02-08 14:16:14 +0900

    Hack only android

--- src/components/ScrollableTabs/ExScrollableTabBar.js
+++ src/components/ScrollableTabs/ExScrollableTabBar.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import ReactNative from 'react-native';
 
 import { DefaultTabBar } from 'react-native-scrollable-tab-view';
+
+const { Platform } = ReactNative;
 
 export default class ExScrollableTabBar extends DefaultTabBar {
   componentDidMount() {
     // NOTE AndroidでTopページが初期表示されない不具合に対するHackコード
-    this.props.goToPage(-1);
-    setTimeout(() => this.props.goToPage(1), 0);
+    if (Platform.OS === 'android') {
+      this.props.goToPage(-1);
+      setTimeout(() => this.props.goToPage(1), 0);
+    }
   }
 }
