946ae69d3b080a328ca24885293c3627b5ba4d6a
norikt <norikt@gmail.com>
2017-03-01 14:43:15 +0900

    Relinking vector-icons

--- android/app/src/main/java/com/sharewis/ShareWis/MainApplication.java
+++ android/app/src/main/java/com/sharewis/ShareWis/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.facebook.CallbackManager;
@@ -40,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new OrientationPackage(),
           new ReactNativeI18n(),
           new FBSDKPackage(mCallbackManager),
