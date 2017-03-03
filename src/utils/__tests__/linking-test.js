1aa48d117f13a44da1ce35eade3de69fb953ee55
norikt <norikt@gmail.com>
2017-03-03 15:10:42 +0900

    Update devDependencies

--- src/utils/__tests__/linking-test.js
+++ src/utils/__tests__/linking-test.js
@@ -1,18 +1,24 @@
 /* eslint-disable no-undef */
+/* eslint-disable global-require */
 import redirectTo from '../linking';
-import { ACT_API_URL } from '../../constants/Api';
 
 global.Promise = require.requireActual('promise');
 
-jest.mock('Linking', () => ({
-  canOpenURL: url =>
-    new Promise((resolve) => {
-      resolve(url === ACT_API_URL);
-    }),
-  openURL: url => new Promise(resolve => resolve(url)),
-}));
+jest.mock('Linking', () => {
+  const ACT_API_URL = require('../../constants/Api');
+
+  return {
+    canOpenURL: url =>
+      new Promise((resolve) => {
+        resolve(url === ACT_API_URL);
+      }),
+    openURL: url => new Promise(resolve => resolve(url)),
+  };
+});
 
 describe('Linking Utils', () => {
+  const ACT_API_URL = require('../../constants/Api');
+
   it('should redirect to the given url', () =>
     redirectTo(ACT_API_URL).then(result =>
       expect(result).toEqual(ACT_API_URL),
