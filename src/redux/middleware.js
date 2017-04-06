6c59f3c83f0be676e7b7890a4fe0db82dbbe10c5
norikt <norikt@gmail.com>
2017-04-06 12:27:03 +0900

    Extract middleware

---
+++ src/redux/middleware.js
@@ -0,0 +1,13 @@
+import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import mixpanelMiddleware from './middleware/mixpanel';
+import netInfoMiddleware from './middleware/netInfo';
+
+const loggerMiddleware = createLogger();
+
+export default [
+  thunkMiddleware,
+  netInfoMiddleware,
+  mixpanelMiddleware,
+  loggerMiddleware,
+];
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,24 +3,14 @@
 /* eslint no-console: ["error", { allow: ["log"] }] */
 
 import { applyMiddleware, compose, createStore } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
 import Reactotron from 'reactotron-react-native';
 import createReactotronEnhancer from 'reactotron-redux';
 import { autoRehydrate } from 'redux-persist';
 
-import netInfoMiddleware from './middleware/netInfo';
-import mixpanelMiddleware from './middleware/mixpanel';
 import rootReducer from '../modules/reducers';
+import middleware from './middleware';
 
-
-const loggerMiddleware = createLogger();
-const middleWare = applyMiddleware(
-  thunkMiddleware,
-  netInfoMiddleware,
-  mixpanelMiddleware,
-  loggerMiddleware,
-);
+const middleWare = applyMiddleware(...middleware);
 
 const configureStore = (initialState) => {
   let composition = compose(middleWare, autoRehydrate());
