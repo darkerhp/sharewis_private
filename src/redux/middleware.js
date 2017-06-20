f7023d82fbf59f161e7aa9591f0511c8f4147aba
norikt <norikt@gmail.com>
2017-06-20 12:02:01 +0900

    Upgrade react native and packages

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,4 +1,4 @@
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import mixpanelMiddleware from './middleware/mixpanel';
 import netInfoMiddleware from './middleware/netInfo';
