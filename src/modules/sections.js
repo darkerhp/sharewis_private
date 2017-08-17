031576148cd3ee0395109a3900472f2bdeba7f16
norikt <norikt@gmail.com>
2017-08-17 18:30:17 +0900

    1737695-150285772 セクションが表示されなくなっている

--- src/modules/sections.js
+++ src/modules/sections.js
@@ -5,7 +5,7 @@ import { fromJS } from 'immutable';
 import { handleActions } from 'redux-actions';
 import { REHYDRATE } from 'redux-persist/constants';
 
-
+import { FETCH_COURSE_DETAILS_SUCCESS } from './lectures';
 import Lecture from './models/Lecture';
 import LectureMap from './models/LectureMap';
 
@@ -18,10 +18,11 @@ const mergeEntities = (state, newSections) =>
   state.merge(newSections.map(section => new Lecture(section)));
 
 const reducer = handleActions({
-  FETCH_COURSE_DETAILS_SUCCESS: (state, action) => {
+  [FETCH_COURSE_DETAILS_SUCCESS]: (state, action) => {
     const sections = action.payload.entities.sections;
     if (!sections) return state;
-    return mergeEntities(state, fromJS(sections));
+    const next = mergeEntities(state, fromJS(sections));
+    return next;
   },
   // redux-persistのrehydrate用のreducer
   // Immutable.jsを使用する場合、変換が必要
