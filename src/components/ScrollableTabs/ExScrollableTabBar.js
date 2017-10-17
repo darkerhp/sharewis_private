d221c1a412a5a28fc613f956c6e45fc7eab530fa
norikt <norikt@gmail.com>
2017-10-17 17:28:03 +0900

    Fix to Not displayed Top on ios

--- src/components/ScrollableTabs/ExScrollableTabBar.js
+++ src/components/ScrollableTabs/ExScrollableTabBar.js
@@ -8,9 +8,8 @@ const { Platform } = ReactNative;
 export default class ExScrollableTabBar extends ScrollableTabBar {
   componentDidMount() {
     // NOTE AndroidでTopページが初期表示されない不具合に対するHackコード
-    if (Platform.OS === 'android') {
-      this.props.goToPage(-1);
-      setTimeout(() => this.props.goToPage(1), 0);
-    }
+    // RN 0.48にアップデート後 iosでも発症
+    this.props.goToPage(-1);
+    setTimeout(() => this.props.goToPage(1), 0);
   }
 }
